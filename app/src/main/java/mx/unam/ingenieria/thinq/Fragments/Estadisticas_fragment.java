c82079b4ad40b49e8399b1b90463f343c14f8eb5
gamalielrios <raulgamalielrios@gmail.com>
2021-07-15 23:55:32 -0500

    Add a new script Java

---
+++ app/src/main/java/mx/unam/ingenieria/thinq/Fragments/Estadisticas_fragment.java
@@ -0,0 +1,23 @@
+package mx.unam.ingenieria.thinq.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import mx.unam.ingenieria.thinq.R;
+
+public class Estadisticas_fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View view=inflater.inflate(R.layout.estadisticas_fragment,container,false);
+        return view;
+    }
+}
