e317ca4a16490083828f90c9631f027f43fc5dc8
LumCor <manuel10fakepm@gmail.com>
2021-07-26 11:03:15 -0500

    notas de voz

--- app/src/main/java/mx/unam/ingenieria/thinq/Fragments/Notas_fragment.java
+++ app/src/main/java/mx/unam/ingenieria/thinq/Fragments/Notas_fragment.java
@@ -4,18 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
+import mx.unam.ingenieria.thinq.Adaptadores.Notas_Adaptador;
 import mx.unam.ingenieria.thinq.R;
 
-public class Notas_fragment  extends Fragment {
+public class Notas_fragment  extends Fragment
+{
+    ListView lista;
+    EditText txtTitulo,txtContenido;
+    Notas_Adaptador notas_adaptador;
+    List<String> item=null;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.notas_fragment,container,false);
+lista=view.findViewById(R.id.Lista_notas);
         return view;
     }
 }
