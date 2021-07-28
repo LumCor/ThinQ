34bef67073a7f7b43e3d565eccf19babe49198d9
LumCor <manuel10fakepm@gmail.com>
2021-07-27 19:04:08 -0500

    bloc de notas

--- app/src/main/java/mx/unam/ingenieria/thinq/Activity_Nota.java
+++ app/src/main/java/mx/unam/ingenieria/thinq/Activity_Nota.java
@@ -1,8 +1,23 @@
 package mx.unam.ingenieria.thinq;
 
 import android.app.ActionBar;
+import android.os.Bundle;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_Nota extends AppCompatActivity {
+    Button Add;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nueva_nota);
+
+        Add=(Button)findViewById(R.id.btAgregar);
+
+        Bundle bundle=this.getIntent().getExtras();
+        if(bundle.getString("llave").equals("add"))
+            Add.setText("Add nota");
+    }
 }
