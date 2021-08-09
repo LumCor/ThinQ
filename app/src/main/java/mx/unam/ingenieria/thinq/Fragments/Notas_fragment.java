1df3449eab75714162e36ce6241ccae354414479
LumCor <manuel10fakepm@gmail.com>
2021-08-08 20:04:58 -0500

    bloc de notas

--- app/src/main/java/mx/unam/ingenieria/thinq/Fragments/Notas_fragment.java
+++ app/src/main/java/mx/unam/ingenieria/thinq/Fragments/Notas_fragment.java
@@ -1,8 +1,13 @@
 package mx.unam.ingenieria.thinq.Fragments;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,8 +19,18 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,7 +53,7 @@ public class Notas_fragment  extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.notas_fragment,container,false);
         lista=view.findViewById(R.id.Lista_notas);
-        notas.add(new Ficha_Nota("Cita Oxxo","9:30, enfrente de la veterinaria"));
+        //CargarNotas();
         lista.setAdapter(new Notas_Adaptador(getContext(),notas));
         setHasOptionsMenu(true);
         return view;
@@ -51,10 +66,61 @@ public class Notas_fragment  extends Fragment
             if(resultCode==Activity.RESULT_OK)
             {
                 notas.add(new Ficha_Nota(data.getExtras().getString("titulo"),data.getExtras().getString("contenido")));
-                lista.setAdapter(new Notas_Adaptador(getContext(),notas));
+                GuardarNotas();
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void CargarNotas()
+    {
+        try
+        {
+        String aux=String.valueOf(getContext().getExternalFilesDir(Environment.DIRECTORY_DCIM))+"/Notas.txt";
+        File directorio= new File(aux);
+        BufferedReader reader=new BufferedReader((new InputStreamReader(new FileInputStream(directorio))));
+        int i=Integer.parseInt(reader.readLine());
+        reader.close();
+        Log.d("Ojo",String.valueOf(i));
+        } catch (FileNotFoundException e) { e.printStackTrace(); } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //notas.add(new Ficha_Nota("Cita Oxxo","9:30, enfrente de la veterinaria"));
+    }
+    private void GuardarNotas()
+    {
+        lista.setAdapter(new Notas_Adaptador(getContext(),notas));
+        String datos=String.valueOf(notas.size())+System.getProperty("line.separator");
+        for (Ficha_Nota nota:notas)
+        {
+            datos+=nota.getTitle()+","+nota.getTContent()+System.getProperty("line.separator");
+
+        }
+        File directorio= getContext().getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        if(!directorio.exists())
+            directorio.mkdir();
+        try
+        {
+            File file = new File(directorio, "Notas" + ".txt");
+            if(!file.exists())
+                file.createNewFile();
+            else
+                {
+                file.delete();
+                file.createNewFile();
+            }
+            Log.d("Ojo",String.valueOf(file.getAbsolutePath()));
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},225);
+        else
+            {
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(getContext().openFileOutput("Notas.txt", getContext().MODE_PRIVATE));
+                outputStreamWriter.write(datos);
+                outputStreamWriter.close();
+            }
+        }
+        catch (IOException e) { e.printStackTrace(); }
+
     }
 
     @Override
