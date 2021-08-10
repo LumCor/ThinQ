d5821d51c028588e94b80c25396ad5d311360600
FrankMS369 <frankms2000@outlook.com>
2021-08-09 20:55:30 -0500

    TPendientesRediseño

--- app/src/main/java/mx/unam/ingenieria/thinq/Fragments/Tareas_fragment.java
+++ app/src/main/java/mx/unam/ingenieria/thinq/Fragments/Tareas_fragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,37 +19,61 @@ import mx.unam.ingenieria.thinq.R;
 import mx.unam.ingenieria.thinq.Adaptadores.TPendiente;
 import mx.unam.ingenieria.thinq.Adaptadores.TPendiente_Adaptador;
 
-public class Tareas_fragment extends Fragment {
+public class Tareas_fragment extends Fragment/*implements TPendiente_Adaptador.ItemSelected*/ {
 
+
+    //EditarTarea_fragment editarTarea_fragment;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager manager;
     RecyclerView.Adapter myAdapter;
-    ArrayList<TPendiente> tareas;
+    ArrayList<TPendiente> tarea;
+    Button btnNuevaTarea;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.tareas_fragment,container,false);
+        btnNuevaTarea = view.findViewById(R.id.btnNuevaTarea);
         recyclerView = view.findViewById(R.id.listPendientes);
         recyclerView.setHasFixedSize(true);
         manager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(manager);
-        tareas = new ArrayList<>();
-        tareas.add(new TPendiente("Matematica", "Serie", "facil"));
-        tareas.add(new TPendiente("Ecuaciones", "Investigacion", "masomenos"));
-        tareas.add(new TPendiente("Programacion", "ProyectoAlv :'v", "dificil"));
-        tareas.add(new TPendiente("Matematica", "Serie", "facil"));
-        tareas.add(new TPendiente("Ecuaciones", "Investigacion", "masomenos"));
-        tareas.add(new TPendiente("Programacion", "ProyectoAlv :'v", "dificil"));
-        tareas.add(new TPendiente("Matematica", "Serie", "facil"));
-        tareas.add(new TPendiente("Ecuaciones", "Investigacion", "masomenos"));
-        tareas.add(new TPendiente("Programacion", "ProyectoAlv :'v", "dificil"));
-        tareas.add(new TPendiente("Matematica", "Serie", "facil"));
-        tareas.add(new TPendiente("Ecuaciones", "Investigacion", "masomenos"));
-        tareas.add(new TPendiente("Programacion", "ProyectoAlv :'v", "dificil"));
+        tarea = new ArrayList<>();
+        tarea.add(new TPendiente("Matematica", "Serie", "facil"));
+        tarea.add(new TPendiente("Ecuaciones", "Investigacion", "masomenos"));
+        tarea.add(new TPendiente("Programacion", "ProyectoAlv :'v", "dificil"));
+        tarea.add(new TPendiente("Matematica", "Serie", "facil"));
+        tarea.add(new TPendiente("Ecuaciones", "Investigacion", "masomenos"));
+        tarea.add(new TPendiente("Programacion", "ProyectoAlv :'v", "dificil"));
+        //btnNuevaTarea.setOnClickListener(onClickEditar);
 
-        myAdapter = new TPendiente_Adaptador(getContext(), tareas);
+        btnNuevaTarea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tarea.add(new TPendiente("Materia", "Descripcion","dificil"));
+                myAdapter.notifyDataSetChanged();
+
+            }
+        });
+
+        myAdapter = new TPendiente_Adaptador(getContext(), tarea);
         recyclerView.setAdapter(myAdapter);
         return view;
     }
+
+    /*View.OnClickListener onClickEditar= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            editarTarea_fragment= new EditarTarea_fragment();
+            getChildFragmentManager().beginTransaction().replace(R.id.editarTarea_fragment).commit();
+        }
+    };*/
+
+    /*@Override
+    public void onItemClicked(int index) {
+        Toast.makeText( getContext() , tarea.get(index).getMateria(), Toast.LENGTH_SHORT).show();
+
+    }*/
 }
